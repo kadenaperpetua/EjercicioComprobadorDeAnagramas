cab0d6ec088465a80c878c5c1ad745ffedefc2cb
Elizabeth Velasco <j.elizabeth.v@gmail.com>
2016-08-09 15:35:03 -0500

    ELI: elimina funciones no utilizadas: contar espacios y borrar espacios

--- src/com/eli/ComprobadorDeAnagramas/Main.java
+++ src/com/eli/ComprobadorDeAnagramas/Main.java
@@ -12,18 +12,7 @@ public class Main {
         String entrada4 ="I am Lord Voldemort";
         comprobarAnagrama(entrada3,entrada4);
 
-        //int a= ' ';
-        //System.out.println("a = " + a);
-        //char[] comprobador= normalizar(entrada1);
-        /*
-        for(int i=0;i<comprobador.length;i++) {
-            System.out.print(comprobador[i]);
         }
-        */
-
-
-
-    }
 
     public static char[] normalizar(String entrada){
         entrada=entrada.toLowerCase();
@@ -31,26 +20,6 @@ public class Main {
         return salida;
     }
 
-
-    /*
-    public static int contarEspacios(char[] entrada){
-        int espacios=0;
-        for (char letra:entrada){
-            if (letra == 32){
-                espacios++;
-            }
-
-        }
-        return espacios;
-    }
-
-
-    public static char[] borrarEspacios(char[] entrada){
-        char[] salida = char[entrada.length-contarEspacios(entrada)];
-
-        for(int i=0;i<entrada.length-contarEspacios(entrada);i++);
-
-    }*/
 
     public static int contarInstancias(char[] entrada){
         int instancias=0;
