74fccb4009abc3b888403bc282effbaf90b87dea
Elizabeth Velasco <j.elizabeth.v@gmail.com>
2016-08-09 14:30:38 -0500

    ELI: Agrega funcion normalizar

--- src/com/eli/ComprobadorDeAnagramas/Main.java
+++ src/com/eli/ComprobadorDeAnagramas/Main.java
@@ -3,9 +3,20 @@ package com.eli.ComprobadorDeAnagramas;
 public class Main {
 
     public static void main(String[] args) {
-	    String textoDeOrigen = "protesta";
+	    String textoDeOrigen = "PRO TESTA";
         String textoPorComprobar ="portaste";
         
+        //int a= ' ';
+        //System.out.println("a = " + a);
+
+        char[] comprobador= normalizar(textoDeOrigen);
+        System.out.println("comprobador = " + comprobador);
+    }
+
+    public static char[] normalizar(String entrada){
+        entrada=entrada.toLowerCase();
+        char[] salida=entrada.toCharArray();
+        return salida;
 
     }
 }
