d766d93cf4d3e9a2d201d2318d183fc54c5ac0a7
Elizabeth Velasco <j.elizabeth.v@gmail.com>
2016-08-09 16:09:11 -0500

    ELI: agrega comprobador de tercera condicion

--- src/com/eli/ComprobadorDeAnagramas/Main.java
+++ src/com/eli/ComprobadorDeAnagramas/Main.java
@@ -7,10 +7,16 @@ public class Main {
         String entrada2 ="portaste";
         comprobarAnagrama(entrada1,entrada2);
 
-
+/*
         String entrada3 = "Tom Marvolo Riddle";
         String entrada4 ="I am Lord Voldemort";
         comprobarAnagrama(entrada3,entrada4);
+
+        String entrada5 = "abef";
+        String entrada6 ="acdf";
+        comprobarAnagrama(entrada3,entrada4);
+
+*/
 
         }
 
@@ -36,26 +42,59 @@ public class Main {
         char[] entrada1norm = normalizar(entrada1);
         char[] entrada2norm = normalizar(entrada2);
 
+
+        //Condicion 1: La sumatoria de valores UNICODE de todas las letras debe ser la misma en ambos textos.
         int sumatoria1 = 0;
         int sumatoria2 = 0;
+
+
+        //Condicion 2: La cantidad de letras debe ser la misma en ambos textos.
         int contador1 = 0;
         int contador2 = 0;
+
+        //condicion3: Todas las letras deben estar presentes al menos una vez en cada texto.
+        int comprobador = 0;
+
 
         for (char letra:entrada1norm) {
             if (letra != 32) {
                 sumatoria1+= letra;
                 contador1++;
+
+
+                for (int i=0;i<entrada2norm.length;i++){
+                    if (entrada2norm[i]==letra) {
+                        comprobador++ ;
+                        break;
+                    }
+
+                }
+
             }
+
         }
 
-        for (char letra:entrada1norm){
+
+
+        for (char letra:entrada2norm){
             if (letra != 32){
                 sumatoria2+= letra;
                 contador2++;
             }
         }
 
-        if(sumatoria1==sumatoria2 && contador1==contador2){
+
+        System.out.println("sumatoria1 = " + sumatoria1);
+        System.out.println("sumatoria2 = " + sumatoria2);
+        System.out.println("contador1 = " + contador1);
+        System.out.println("contador2 = " + contador2);
+        System.out.println("comprobador = " + comprobador);
+
+
+
+
+
+        if(sumatoria1==sumatoria2 && contador1==contador2 && contador1==comprobador){
             System.out.println("El texto es un anagrama");
         }else{
             System.out.println("El texto no es un anagrama");
