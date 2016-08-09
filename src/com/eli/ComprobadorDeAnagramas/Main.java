3fbbb079e462e8118f24398c3848ee6b9d39547a
Elizabeth Velasco <j.elizabeth.v@gmail.com>
2016-08-09 14:08:52 -0500

    ELI: Agrega strings como datos de entrada

--- src/com/eli/ComprobadorDeAnagramas/Main.java
+++ src/com/eli/ComprobadorDeAnagramas/Main.java
@@ -3,6 +3,9 @@ package com.eli.ComprobadorDeAnagramas;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    String textoDeOrigen = "protesta";
+        String textoPorComprobar ="portaste";
+        
+
     }
 }
